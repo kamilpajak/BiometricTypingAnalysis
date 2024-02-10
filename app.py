900ecbd9d81c9d991952c9184a7e8d420ed1b596
Kamil Pajak <kontakt@kamilpajak.pl>
2024-02-10 19:56:53 +0100

    Add user registration/login/logout

--- app.py
+++ app.py
@@ -1,14 +1,92 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for, flash, g, session
+from sqlalchemy.exc import IntegrityError
 
+from extensions import db
 from feature_calculator import FeatureCalculator
+from forms import RegistrationForm, LoginForm
 from keystroke_processor import KeystrokeProcessor
+from models.models import User
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Or another database URI
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+
+@app.before_request
+def create_tables():
+    if not hasattr(g, 'db_created'):
+        db.create_all()
+        # Remove the handler so it only runs once
+        app.before_request_funcs[None].remove(create_tables)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'user_id' in session:
+        return render_template('index.html')
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # Check whether user already exists
+        user_by_email = User.query.filter_by(email=form.email.data).first()
+        if user_by_email:
+            flash('Email already registered. Please log in.', 'danger')
+            return redirect(url_for('login'))
+
+        user_by_username = User.query.filter_by(username=form.username.data).first()
+        if user_by_username:
+            flash('Username already taken. Please choose a different one.', 'danger')
+            return redirect(url_for('register'))
+
+        # Create a new user with the form data
+        new_user = User(username=form.username.data, email=form.email.data)
+        new_user.set_password(form.password.data)
+
+        # Add the new user to the database
+        db.session.add(new_user)
+        try:
+            db.session.commit()
+            flash('Account created successfully! You can now log in.', 'success')
+            return redirect(url_for('login'))
+        except IntegrityError:  # Catching a specific SQLAlchemy IntegrityError
+            db.session.rollback()
+            flash('There was an issue creating the account. Please try again.', 'danger')
+            return redirect(url_for('register'))
+        except Exception as e:  # Catching any other exceptions that might occur
+            db.session.rollback()
+            flash(f'An error occurred: {e}', 'danger')
+            return redirect(url_for('register'))
+
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            session['user_id'] = user.id
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    session.clear()  # This clears all data stored in the session
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('login'))
 
 
 @app.route('/analyze_keystrokes', methods=['POST'])
